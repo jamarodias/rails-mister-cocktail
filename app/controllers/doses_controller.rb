74ff7083caf612e812bacfa126cfdb5e9cbd4df4
joaoamaro <jamarodias@gmail.com>
2020-09-23 14:27:45 -0300

    avançando mais um pouco

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,8 @@
 class DosesController < ApplicationController
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
     def create
         @cocktail = Cocktail.find(params[:cocktail_id])
         @dose = Dose.new(dose_params)
